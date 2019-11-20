4b9773cb459a47855bdacc6d317e890bd8319330
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-20 09:15:55 +0100

    migraciones hechas

---
+++ database/seeds/shops.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class shops extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
